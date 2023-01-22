36cbd894b2f499264f8e967e8536302c405a32ed
OlgaKG <flory_oli@mail.ru>
2023-01-22 22:34:54 +0300

    fixed errors after 1 review

--- vendor/scripts/index.js
+++ vendor/scripts/index.js
@@ -1,37 +1,31 @@
-const popupBtnOpen = document.querySelector(".profile__edit-btn");
-const popupContainer = document.querySelector(".popup");
-const popupBtnClose = document.querySelector(".popup__closed-btn");
-let formEditBio = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-name");
-let professionInput = document.querySelector(".popup__input-profession");
-let profileName = document.querySelector(".profile__name");
-let profileProfession = document.querySelector(".profile__profession");
+const popupBtnOpen = document.querySelector('.profile__edit-btn');
+const popupContainer = document.querySelector('.popup');
+const popupBtnClose = document.querySelector('.popup__closed-btn');
+let formEditBio = document.querySelector('.popup__form');
+let nameInput = document.querySelector(`input[name='user']`);
+let professionInput = document.querySelector(`input[name='profession']`);
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
 
 function openPopup() {
-    popupContainer.classList.add("popup_opened");
+    popupContainer.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    professionInput.value = profileProfession.textContent;
 }
 
 function closePopup() {
-    popupContainer.classList.remove("popup_opened");
+    popupContainer.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = professionInput.value;
-    popupContainer.classList.remove("popup_opened");
-}
-
-popupBtnOpen.addEventListener("click", openPopup);
-popupBtnClose.addEventListener("click", closePopup);
-popupContainer.addEventListener("click", function(event) {
-    console.log(event.target);
-    console.log(event.currentTarget);
-
-if (event.target === event.currentTarget) {
     closePopup();
 }
-});  
+
+popupBtnOpen.addEventListener('click', openPopup);
+popupBtnClose.addEventListener('click', closePopup);
 
 formEditBio.addEventListener('submit', handleFormSubmit); 
 
