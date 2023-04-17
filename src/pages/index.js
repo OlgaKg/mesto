03048972cef0fa9da463ce62380f72cabf0788de
OlgaKG <flory_oli@mail.ru>
2023-04-17 16:02:26 +0300

    Configure the Webpark build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
+import './index.css';
 import initialCards from '../components/initialCards.js';
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
-import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
